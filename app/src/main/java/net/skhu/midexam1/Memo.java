134029a752946367ff481c088b8cad81848b6d71
ccomapjw <ccomapjw@naver.com>
2020-05-06 17:14:49 +0900

    pjw

--- app/src/main/java/net/skhu/midexam1/Memo.java
+++ app/src/main/java/net/skhu/midexam1/Memo.java
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 //Serializable는 구현할 수 없습니다. 안에 함수가 없는 것이 맞는 표현입니다.
-public class Memo implements Serializable {//Serializable interface 는 marking interface 입니다. 이거를 붙이거나, 이것
+public class Memo implements Serializable {//Serializable interface 는 marking interface 입니다
     String body;//내용
 
     public Memo(String body) {
